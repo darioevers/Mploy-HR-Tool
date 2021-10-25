09b927ab2d70ebb423ff91bd5dffa3a4d5ec49cd
Alex Albaran <albaranlex@icloud.com>
2021-10-25 17:14:43 +0200

    added components: homeoffice + corresponding scss

--- client/src/components/dashboard/maindashboard/widgets/WidgetHomeOffice.js
+++ client/src/components/dashboard/maindashboard/widgets/WidgetHomeOffice.js
@@ -1,6 +1,40 @@
 import React from "react";
 
 function WidgetHomeOffice() {
-  return <div className="widget_homeoffice_mainbox"></div>;
+  return (
+    <div className="widget_homeoffice_mainbox">
+      <div className="homeoffice_top_wrapper">
+        <div className="square_bullet"></div>
+        <h1> Home Office </h1>
+      </div>
+
+      <div className="homeoffice_middle_wrapper">
+        <h4>Available Days</h4>
+        <div className="horizontal_line"></div>
+      </div>
+
+      <div className="homeoffice_bottomleft_wrapper">
+        <h2>21</h2>
+        <h4>Available</h4>
+      </div>
+
+      <div className="homeoffice_bottomleft_line_wrapper">
+        <div className="vertical_line"></div>
+      </div>
+
+      <div className="homeoffice_bottommiddle_wrapper">
+        <h2>10</h2>
+        <h4>Planned</h4>
+      </div>
+
+      <div className="homeoffice_bottommiddle_line_wrapper">
+        <div className="vertical_line"></div>
+      </div>
+      <div className="homeoffice_bottomright_wrapper">
+        <h2>4</h2>
+        <h4>Taken</h4>
+      </div>
+    </div>
+  );
 }
 export default WidgetHomeOffice;
