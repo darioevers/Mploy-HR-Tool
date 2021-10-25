77b6ffd2f45c0111cf945284c2e0cc0675542acd
Alex Albaran <albaranlex@icloud.com>
2021-10-25 17:05:36 +0200

    added components: sick leave, holidays + corresponding scss

--- client/src/components/dashboard/maindashboard/widgets/WidgetHolidays.js
+++ client/src/components/dashboard/maindashboard/widgets/WidgetHolidays.js
@@ -1,6 +1,40 @@
 import React from "react";
 
 function WidgetHolidays() {
-  return <div className="widget_holidays_mainbox"></div>;
+  return (
+    <div className="widget_holidays_mainbox">
+      <div className="holidays_top_wrapper">
+        <div className="square_bullet"></div>
+        <h1> Holidays </h1>
+      </div>
+
+      <div className="holidays_middle_wrapper">
+        <h4>Available Days</h4>
+        <div className="horizontal_line"></div>
+      </div>
+
+      <div className="holidays_bottomleft_wrapper">
+        <h2>21</h2>
+        <h4>Available</h4>
+      </div>
+
+      <div className="holidays_bottomleft_line_wrapper">
+        <div className="vertical_line"></div>
+      </div>
+
+      <div className="holidays_bottommiddle_wrapper">
+        <h2>10</h2>
+        <h4>Planned</h4>
+      </div>
+
+      <div className="holidays_bottommiddle_line_wrapper">
+        <div className="vertical_line"></div>
+      </div>
+      <div className="holidays_bottomright_wrapper">
+        <h2>4</h2>
+        <h4>Taken</h4>
+      </div>
+    </div>
+  );
 }
 export default WidgetHolidays;
