c9c695556e7e463659023b33f41cd9f9aec683c5
MaryamPayenda <maryampayanda12@gmail.com>
2021-10-26 10:15:22 +0200

    added

--- client/src/components/landingpage/login/dashboard.js
+++ client/src/components/landingpage/login/dashboard.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import TopNav from "../../dashboard/global/TopNav";
 
 const PrivateScreen = ({ history }) => {
   const [error, setError] = useState("");
