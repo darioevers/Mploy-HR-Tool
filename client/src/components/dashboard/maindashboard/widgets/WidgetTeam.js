cec7ff759ae17dbde8ca802c5f0b78da8357f19a
Alex Albaran <albaranlex@icloud.com>
2021-10-26 13:59:21 +0200

    added contact carousel + scss

--- client/src/components/dashboard/maindashboard/widgets/WidgetTeam.js
+++ client/src/components/dashboard/maindashboard/widgets/WidgetTeam.js
@@ -1,18 +1,21 @@
 import React from "react";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import InfoIcon from "@material-ui/icons/Info";
+import ContactCarousel from "./ContactsCarousel";
 
 function WidgetTeam() {
   return (
     <div className="widget_team_mainbox">
+      {/* Team Header */}
       <div className="team_header_top">
         <h1>TEAM</h1>
         <div className="horizontal_line"></div>
       </div>
 
+      {/* Manager Wrapper */}
       <div className="team_manager_wrapper">
         <div>
-          <div className="team_manager_photo">IMAGE</div>
+          <div className="team_manager_photo"></div>
           <div className="team_manager_title">
             <h4>Project Mngr.</h4>
           </div>
@@ -27,78 +30,69 @@ function WidgetTeam() {
         </div>
       </div>
 
+      {/* Supervisor */}
       <div className="team_supervisor_wrapper">
         <div>
-          <div className="team_supervisor_photo">IMAGE</div>
+          <div className="team_supervisor_photo"></div>
           <div className="team_supervisor_title">
             <h4>Supervisor</h4>
           </div>
         </div>
         <div>
           <div>
-            <i>
-              <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
-            </i>
+            <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
           <div>
-            <i>
-              <InfoIcon style={{ fontSize: "small", color: "blue" }} />
-            </i>
+            <InfoIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
         </div>
       </div>
 
+      {/* IT Manager / Supervisor */}
       <div className="team_it_one">
         <div>
-          <div className="team_it_one_photo">IMAGE</div>
+          <div className="team_it_one_photo"></div>
           <div className="team_it_one_title">
             <h4>IT Manager</h4>
           </div>
         </div>
         <div>
           <div>
-            <i>
-              <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
-            </i>
+            <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
           <div>
-            <i>
-              <InfoIcon style={{ fontSize: "small", color: "blue" }} />
-            </i>
+            <InfoIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
         </div>
       </div>
 
+      {/* IT Support */}
       <div className="team_it_two">
         <div>
-          <div className="team_it_two_photo">IMAGE</div>
+          <div className="team_it_two_photo"></div>
           <div className="team_it_two_title">
             <h4>IT Support</h4>
           </div>
         </div>
         <div>
           <div>
-            <i>
-              <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
-            </i>
+            <MailOutlineIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
           <div>
-            <InfoIcon style={{ fontSize: "small", color: "red" }} />
+            <InfoIcon style={{ fontSize: "small", color: "blue" }} />
           </div>
         </div>
       </div>
 
+      {/* Contacts Header */}
       <div className="team_header_bottom">
         <h1>CONTACTS</h1>
         <div className="horizontal_line"></div>
       </div>
 
+      {/* Contacts Wrapper */}
       <div className="team_contacts_wrapper">
-        <div>ARROW LEFT</div>
-        <div>Image 1</div>
-        <div>Image 2</div>
-        <div>Image 3</div>
-        <div>ARROW RIGHT</div>
+        <ContactCarousel />
       </div>
     </div>
   );
