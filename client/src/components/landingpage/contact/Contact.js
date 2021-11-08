9f3bc7b3835052a6ea9a537eb2bab4092cd5ebfd
Dario Evers <darioevers@gmail.com>
2021-11-08 22:50:49 +0100

    contact page changes and styling

--- client/src/components/landingpage/contact/Contact.js
+++ client/src/components/landingpage/contact/Contact.js
@@ -5,6 +5,8 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 
 const Contact = () => {
     return (<Box
@@ -13,46 +15,57 @@ const Contact = () => {
             borderRadius: '0px'
         }}
     >
-        <div>
-            <LandingpageTopnav />
-            <div className="homearea">
-                {/* <Container> */}
-                <div className="contact_mainbox">
-                    <div className="contact_textbox">
-                        <Box component="span" sx={{ width: '100%', maxWidth: 500 }}>
-                            <Typography variant="h3" gutterBottom component="div">
-                                h3. Heading
+
+        <LandingpageTopnav />
+        <div className="homearea">
+            {/* <Container> */}
+            <div className="contact_mainbox">
+                <div className="contact_textbox">
+                    <Box component="span" sx={{ width: '100%', maxWidth: 500 }}>
+                        <Typography variant="h3" component="div">
+                            Get In Touch.
                                 </Typography>
-                        </Box>
-                    </div>
-                    <div className="contact_formbox">
-                        <Box
-                            id="boxtest"
-                            component="form"
-                            sx={{
-                                '& > :not(style)': { m: 1, width: '25ch' },
-                            }}
-                            noValidate
-                            autoComplete="off"
-                        >
-                            <TextField id="standard-basic" label="First Name" variant="standard" />
-                            <TextField id="standard-basic" label="Last Name" variant="standard" />
-                            <TextField id="standard-basic" label="Phone" variant="standard" />
-                            <TextField required id="standard-basic" label="E-Mail" variant="standard" />
-                            <TextField required
-                                id="outlined-multiline-flexible"
-                                label="Message"
-                                multiline
-                                maxRows={4}
-                                fullWidth
-                            />
-                        </Box>
-                    </div>
+                        <Typography variant="h6" gutterBottom sx={{ fontWeight: "400" }}>
+                            Lorem Ipsum
+                                </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Lorem Ipsusmdf suiadh kajshd kashdksadh saka
+                                    </Typography>
+                        <Divider variant="middle" />
+                    </Box>
                 </div>
-                {/* </Container> */}
+                <div className="contact_formbox">
+                    <Box
+                        id="boxtest"
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 2, width: '25ch' },
+                            textAlign: "center",
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <TextField required id="contactform_name" label="First Name" variant="standard" />
+                        <TextField required id="contactform_name" label="Last Name" variant="standard" />
+                        <TextField required id="contactform_email" label="Email" variant="standard" />
+                        <TextField id="contactform_phone" label="Phone" variant="standard" />
+                        <TextField required
+                            id="contactform_message"
+                            label="Message"
+                            multiline
+                            maxRows={5}
+                            rows={4}
+                            fullWidth
+                            style={{ width: "90%" }}
+                        />
+                        <Button variant="contained" id="contactform_namelabel" style={{ width: "90%" }}>Send Message</Button>
+                    </Box>
+                </div>
             </div>
-            <LandingpageFooter />
-        </div >
+            {/* </Container> */}
+        </div>
+        <LandingpageFooter />
+
     </Box>)
 }
 
