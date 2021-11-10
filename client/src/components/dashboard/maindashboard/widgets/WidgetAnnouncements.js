015e3924ef562000a390e0259ae519a01e7e0f6f
Alex Albaran <albaranlex@icloud.com>
2021-11-10 11:41:46 +0100

    connected Announcements component to database. get protocol established

--- client/src/components/dashboard/maindashboard/widgets/WidgetAnnouncements.js
+++ client/src/components/dashboard/maindashboard/widgets/WidgetAnnouncements.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function WidgetAnnouncements() {
+  const [announcements, setAnnouncements] = useState();
+  useEffect(() => {
+    getAllAnnouncements();
+  }, []);
+  // fetching data from backend
+  const getAllAnnouncements = () => {
+    axios
+      .get(
+        "http://localhost:5000/announcements/announcements",
+
+        {
+          header: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+
+      .then((data) => {
+        console.log(data.data);
+        setAnnouncements(data.data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="widget_announcements_mainbox">
       <div className="widget_announcements_header">
         <h1>ANNOUNCEMENTS</h1>
       </div>
 
-      <div className="widget_announcements_display">
-        <div className="widget_announcement_one">1</div>
-        <div className="widget_announcement_two">2</div>
-        <div className="widget_announcement_three">3</div>
-      </div>
+      {announcements &&
+        announcements.map((announcement) => (
+          <div className="widget_announcements_display" key={announcement.id}>
+            <div className="widget_announcement_one">{announcement.title}</div>
+          </div>
+        ))}
     </div>
   );
 }
