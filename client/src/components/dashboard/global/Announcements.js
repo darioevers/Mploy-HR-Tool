1b37fb58337fda523397fbe72a200a7579a47004
Alex Albaran <albaranlex@icloud.com>
2021-11-10 01:35:43 +0100

    added new schema, model, controller, route for announcments

--- client/src/components/dashboard/global/Announcements.js
+++ client/src/components/dashboard/global/Announcements.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Announcements() {
-  const submitAnnouncement = () => {
-    alert("Implement Submit!");
-  };
+  const [announcement, setAnnouncement] = useState({});
+
   return (
     <div className="announcements_mainbox">
       <div className="announcements_date">
@@ -20,21 +20,46 @@ function Announcements() {
 
       <div className="announcements_title">
         <h3>Title</h3>
-        <input type="text" placeholder="Enter title..."></input>
+        <input
+          type="text"
+          placeholder="Enter title..."
+          name="title"
+          onChange={(e) =>
+            setAnnouncement({ ...announcement, title: e.target.value })
+          }
+        ></input>
       </div>
 
       <div className="announcements_message">
         <h3>Message</h3>
-        <textarea placeholder="Write message here..."></textarea>
+        <textarea
+          placeholder="Write message here..."
+          name="message"
+          onChange={(e) =>
+            setAnnouncement({ ...announcement, message: e.target.value })
+          }
+        ></textarea>
       </div>
 
       <div className="announcements_poster">
         <h3>Posted By</h3>
-        <input type="text"></input>
+        <input
+          type="text"
+          name="poster"
+          onChange={(e) =>
+            setAnnouncement({ ...announcement, poster: e.target.value })
+          }
+        ></input>
       </div>
 
       <div className="announcements_post">
-        <button onClick={submitAnnouncement}>POST</button>
+        <button
+          onClick={() => {
+            console.log(announcement);
+          }}
+        >
+          POST
+        </button>
       </div>
     </div>
   );
