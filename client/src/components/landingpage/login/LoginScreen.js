bc860933671fe0a6f7e115833ab6610870efe05f
Dario Evers <darioevers@gmail.com>
2021-11-18 22:05:46 +0100

    added language support

--- client/src/components/landingpage/login/LoginScreen.js
+++ client/src/components/landingpage/login/LoginScreen.js
@@ -1,9 +1,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import Box from '@mui/material/Paper';
 
-// IMPORT COMPONENTS
+// MUI IMPORTS
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// COMPONENT IMPORTS
 import LandingpageTopnav from "../LandingpageTopnav";
 
 const LoginScreen = ({ history, location }) => {
@@ -54,11 +67,86 @@ const LoginScreen = ({ history, location }) => {
       <LandingpageTopnav />
       <Box
         sx={{
-          bgcolor: 'landingnavbar.main',
-          borderRadius: '0px'
+          bgcolor: "landingnavbar.main",
+          borderRadius: "0px",
         }}
       >
-        <div className="loginarea">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={loginHandler}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link to="/forgotpassword" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link to="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </div>
+  );
+};
+
+export default LoginScreen;
+
+{
+  /* <div className="loginarea">
           <div className="login-screen">
             <form onSubmit={loginHandler} className="login-screen__form">
               <h3 className="login-screen__title"> Sign in </h3>
@@ -81,7 +169,7 @@ const LoginScreen = ({ history, location }) => {
                     className="login-screen__forgotpassword"
                   >
                     Forgot Password?
-                </Link>
+                  </Link>
                 </label>
                 <input
                   type="password"
@@ -96,17 +184,12 @@ const LoginScreen = ({ history, location }) => {
               </div>
               <button type="submit" className="btn btn-primary">
                 Login
-            </button>
+              </button>
 
               <span className="login-screen__subtext">
                 Don't have an account? <Link to="/register">Register</Link>
               </span>
             </form>
           </div>
-        </div>
-      </Box>
-    </div>
-  );
-};
-
-export default LoginScreen;
+        </div> */
+}
