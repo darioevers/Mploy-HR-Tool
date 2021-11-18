ab7083d2cca2de110efdf2459c81f662acfe650c
MaryamPayenda <maryampayanda12@gmail.com>
2021-11-18 12:49:28 +0100

    edit upload profile photo added

--- client/src/components/dashboard/employeedata/EmployeeData.js
+++ client/src/components/dashboard/employeedata/EmployeeData.js
@@ -127,7 +127,7 @@ function EmployeeData({ history }) {
                 >
                   <TableCell>
                     <img
-                      src={`http://localhost:5000/${employee.bio.photo}`} onError={(e)=>{e.target.onError=null; e.target.src='http://localhost:5000/uploads/error.png'}} />
+                      src={`http://localhost:5000/${employee.bio.photo}`} onError={(e)=>{e.target.onError=null; e.target.src='http://localhost:5000/uploads/error.jpg'}} />
                      
                   </TableCell>
 
